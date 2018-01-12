b8fe3301f69e09d99b9a976f569f2530cb5155c7
Amira AYADI <ayadi.amira01@gmail.com>
2018-01-12 13:08:55 +0100

    Maj
    Footer == OK
    Header == OK

---
+++ Visuel2/Header/header.js
@@ -0,0 +1,26 @@
+/*fonction servant a inserer le header dans l'ensemble des pages du site. Permet d'actualiser tous les headers en meme temps.*/
+/*utilise le lien vers bootstrap de chaque page */
+
+document.body.insertAdjacentHTML("afterBegin",
+'<header>'+
+    '<nav class="navbar navbar-default">'+     
+      '<div id = "navBar" class="container">'+
+        '<div class="navbar-header">'+
+          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#codebrainery-toggle-nav" aria-expanded="false">'+
+           '<span class="sr-only">Toggle navigation </span>'+
+           '<span class="icon-bar"></span>'+
+           '<span class="icon-bar"></span>'+
+           '<span class="icon-bar"></span>'+
+         '</button>'+
+         '<img src="https://raw.githubusercontent.com/ProjetSID2018/Groupe10_Qualite_communication/master/Logos/bleu1.png" class="navbar-brand"></img>'+
+        '</div>'+
+        '<div class="collapse navbar-collapse" id="codebrainery-toggle-nav">'+
+         '<ul class="nav navbar-nav navbar-right">'+
+           '<li class="item"><a  href="#">Accueil</a></li>'+
+           '<li class="item"><a  href="#">Thèmes</a></li>'+
+           '<li class="item"><a  href="#">Recherche</a></li>'+
+         '</ul>'+
+        '</div>'+
+      '</div>'+
+    '</nav>'+
+  '</header>');
